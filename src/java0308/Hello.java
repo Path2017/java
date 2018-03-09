f6bedece8d200e6cb470b35358f5b0f3e3d34cd4
pathliu <path2017@126.com>
2018-03-09 15:56:08 +0800

    add java demo

---
+++ src/java0308/Hello.java
@@ -0,0 +1,20 @@
+package java0308;
+
+import java.util.Scanner;
+
+public class Hello {
+
+	private static Scanner in;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int foot;
+		double inch;
+		in = new Scanner(System.in);
+		foot = in.nextInt();
+		inch = in.nextDouble();
+		System.out.println(foot);
+		System.out.println(inch);
+	}
+
+}
--- src/java0308/Main.java
+++
@@ -1,5 +0,0 @@
-package java0308;
-
-public class Main {
-
-}
