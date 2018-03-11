f387a33de6a89133a22f271169f16ccf78ab5acc
pathliu <path2017@126.com>
2018-03-11 15:15:56 +0800

    finish 第一周java题

--- src/java0308/Hell.java
+++ src/java0308/Hell.java
@@ -1,10 +1,22 @@
 package java0308;
 
+import java.util.Scanner;
+
 public class Hell {
+
+	private static Scanner in;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("hello world");
+		int foot;
+		double inch;
+		in = new Scanner(System.in);
+		System.out.println("请输入尺数：");
+		foot = in.nextInt();
+		System.out.println("请输入寸数：");
+		inch = in.nextDouble();
+		System.out.println("身高为(米)：");
+		System.out.println((foot+inch/12)*0.3048);
 	}
 
 }
---
+++ src/java0308/Main.java
@@ -0,0 +1,17 @@
+package java0308;
+
+import java.util.Scanner;
+
+public class Main {
+
+	private static Scanner in;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int f;
+		in = new Scanner(System.in);
+		f = in.nextInt();
+		System.out.println((int)((f-32)*(5/9.0)));
+	}
+
+}
