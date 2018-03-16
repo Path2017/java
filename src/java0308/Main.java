4f98b54d3325469d55171b54d0e54f2e30761842
pathliu <path2017@126.com>
2018-03-16 18:50:52 +0800

    add 计算java方法

---
+++ src/java0308/Main.java
@@ -0,0 +1,24 @@
+package java0308;
+
+import java.util.Scanner;
+
+public class Main {
+	public static Scanner in;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int time;
+		int num;
+		in = new Scanner(System.in);
+		time = in.nextInt();
+		if(time > 800) {
+			time = time - 800;
+		}else if(time < 800){
+			 num = 800 - time;
+			 time = 2400 - num;
+		}else {
+			time = 0;
+		}
+		System.out.println(time);
+	}
+
+}
---
+++ src/java0308/compute.java
@@ -0,0 +1,19 @@
+package java0308;
+
+public class compute {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int limit = 100;
+		int num1 = 15;
+		int num2 = 40;
+		if ( limit <= limit)
+		{
+		    if ( num1 == num2 )
+		        System.out.println("lemon");
+		    System.out.println("lime");
+		}
+		System.out.println("grape");
+	}
+
+}
