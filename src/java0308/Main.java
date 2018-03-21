a100909fca856a80eaf17e9488852c844ae445cd
pathliu <path2017@126.com>
2018-03-21 17:47:38 +0800

    add 奇偶个数

---
+++ src/java0308/Main.java
@@ -0,0 +1,24 @@
+package java0308;
+
+import java.util.Scanner;
+
+public class Main {
+	public static Scanner in;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num;
+		int num1 = 0;
+		int num2 = 0;
+		in = new Scanner(System.in);
+		num = in.nextInt();
+		while(!(num == -1)) {
+			if(num%2 == 0) {
+				num2 = num2 +1;
+			}else {
+				num1 = num1 +1;
+			}
+			num = in.nextInt();
+		}
+		System.out.println(num1+" "+num2);
+	}
+}
---
+++ src/java0308/test.java
@@ -0,0 +1,17 @@
+package java0308;
+
+import java.util.Scanner;
+
+public class test {
+	
+	public static Scanner in;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int mark;
+		in = new Scanner(System.in);
+		mark = in.nextInt();
+		System.out.println(mark);
+
+	}
+
+}
